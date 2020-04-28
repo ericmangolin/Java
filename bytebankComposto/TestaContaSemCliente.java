f5f24d958a6ac31c5e2a82d71447720a7ac6cf24
Eric Mangolim <58992526+ericmangolin@users.noreply.github.com>
2020-04-28 17:10:22 -0300

    Update TestaContaSemCliente.java
    Atribuições diretas para atributo 'titular.nome'

--- bytebankComposto/TestaContaSemCliente.java
+++ bytebankComposto/TestaContaSemCliente.java
@@ -1,9 +1,11 @@
-
 public class TestaContaSemCliente {
 	public static void main(String[] args) {
 		Conta contaEric = new Conta();
 		System.out.println(contaEric.saldo);
-
+		
+		contaEric.titular = new cliente();
+		System.out.println(contaEric.titular);
+		
 		contaEric.titular.nome = "Eric Andre";
 		System.out.println(contaEric.titular.nome);
 	}
