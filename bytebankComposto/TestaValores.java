ffa41b4fab9ddb3c271adc1064b4b0c6d8aa803c
Eric Mangolim <58992526+ericmangolin@users.noreply.github.com>
2020-04-29 19:34:00 -0300

    Utilização de Construtores
    Utiliza construtores para informar parâmetros e definir métodos com base nas regras de negócio

---
+++ bytebankComposto/TestaValores.java
@@ -0,0 +1,13 @@
+
+public class TestaValores {
+	public static void main(String[] args) {
+		Conta conta = new Conta(1779, 13522);
+		
+		//conta.setAgencia(-50);
+		//conta.setNumero(-330);
+		
+		//Brecha erro, inconsistência, retorna zero
+		//System.out.println(conta.getAgencia());
+	}
+
+}
